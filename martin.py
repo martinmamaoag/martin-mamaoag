6cdab2ed87e7e715aa04209fcab0bb0ee75cba02
JaydeeMoles <jmoles22a-0092@olopsc.edu.ph>
2024-03-06 15:06:57 +0800

    first commit

---
+++ martin.py
@@ -0,0 +1,23 @@
+
+count = 0
+while count < 5:
+    print(f"Count: {count}")
+    count += 1
+
+for i in range(1, 5):
+    print(f"Number: {i}")
+
+x = 10
+if x > 0:
+    print("Positive number")
+else:
+    print("Non-positive number")
+
+
+grade = 85
+if grade >= 90:
+    print("A")
+elif 80 <= grade < 90:
+    print("B")
+else:
+    print("C or below")
